d5b25b832daac4daa2c44b5ffe2f4025211c02c1
Alessio Leo <a.leo88@studenti.unisa.it>
2024-12-10 18:02:30 +0100

    .

--- src/html/PaginaBianca.php
+++ src/html/PaginaBianca.php
@@ -4,14 +4,20 @@
     <meta charset="UTF-8">
     <title>Title</title>
     <style>
-        body{
-            background-color: grey;
+        body {
+            background: #170938; /* Un colore uniforme viola scuro */
+        }
+        body {
+            background-image: radial-gradient(189.06% 295.94% at 82.88% 209.58%, rgba(21, 42, 244, 0.15) 0%, rgba(21, 42, 244, 0) 100%),
+            radial-gradient(283.64% 307.92% at 48.06% 222.29%, rgba(106, 24, 247, 0.15) 0%, rgba(106, 24, 247, 0) 100%),
+            radial-gradient(73.96% 449.37% at 88.44% -34.38%, rgba(200, 10, 251, 0) 100%);
+
+            background-image: linear-gradient(rgb(14,11,19), rgb(39,17, 74));
         }
     </style>
     <meta name="Author" content="Gruppo05"/>
     <base href="../"/>
     <link rel="icon" href="img/favicon.png" type="image/png/">
-    <link rel="stylesheet" href="css/TopMenu.css"/>
     <script src="js/TopMenu.js"></script>
 </head>
 <body>
