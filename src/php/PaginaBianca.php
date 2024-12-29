38cd07dac473d37036079c03afb75cfcd8539695
Alessio Leo <a.leo88@studenti.unisa.it>
2024-12-29 22:02:08 +0100

    topbar credo finita tranne i link alle varie sezioni. footer forse finito pagina supporto da finire ma funzionante

---
+++ src/php/PaginaBianca.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <style>
+        html, body { /*forzo il full screen*/
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+        body {
+            background-image: linear-gradient(75deg, rgb(14,11,19), rgb(82, 0, 124));
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    </style>
+    <meta name="Author" content="Gruppo05"/>
+    <base href="../"/>
+    <link rel="icon" href="img/favicon.png" type="image/png/">
+    <script src="js/TopMenu.js"></script>
+</head>
+<body>
+<!--mi sa che il base href vale solo per html e non php-->
+<?php include_once '../html/TopMenu.html'?>
+<?php include_once '../html/Footer.html'?>
+</body>
+</html>
