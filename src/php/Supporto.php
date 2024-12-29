20fd12b0244f84788739e203a72ae8f0c0aac999
Alessio Leo <a.leo88@studenti.unisa.it>
2024-12-29 22:01:50 +0100

    topbar credo finita tranne i link alle varie sezioni. footer forse finito pagina supporto da finire ma funzionante

--- src/php/Supporto.php
+++ src/php/Supporto.php
@@ -3,28 +3,34 @@
     <meta charset="UTF-8">
     <title>Title</title>
     <style>
-        body {
-            background: #170938;
+        html, body { /*forzo il full screen*/
+            height: 100%;
+            margin: 0;
+            padding: 0;
         }
         body {
             background-image: linear-gradient(75deg, rgb(14,11,19), rgb(82, 0, 124));
-            height: 900px;
             background-repeat: no-repeat;
+            background-size: cover;
+        }
+        #text_container{
+            display: block;
+            width: 40vw;
         }
         .text_support{
             color: white;
             display: block;
             font-size: 1.2em;
-            width: 40%;
+            width: auto;
             text-align: center;
-            padding-bottom: 2%;
+            padding-bottom: 3%;
             font-family: -apple-system, BlinkMacSystemFont, sans-serif;
         }
         input, textarea{
             display: block;
             margin-bottom: 1%;
-            height: 5%;
-            width: 40%;
+            height: 5vh;
+            width: 40vw;
             border-radius: 7px;
             border: 1px solid white;
         }
@@ -41,22 +47,21 @@
     <script src="js/TopMenu.js"></script>
 </head>
 <body>
-<?php include_once 'TopMenu.html'?>
+<?php include_once '../html/TopMenu.html' ?>
 <main style="width: 90%; margin-left: 33%;">
-    <div class="text_support">
-        Hai bisogno di aiuto?
-        Siamo qui per rispondere a ogni tua domanda!
-    </div>
+    <div id="text_container">
+        <div class="text_support">
+            Hai bisogno di aiuto?<br>Siamo qui per rispondere a ogni tua domanda!
+        </div>
 
-    <div class="text_support">
-        Che tu stia cercando informazioni sui nostri viaggi spaziali, abbia dubbi sulle prenotazioni o desideri segnalare un problema, il nostro team di supporto è pronto ad assisterti.
-    </div>
+        <div class="text_support">
+            Che tu stia cercando informazioni sui nostri viaggi spaziali, abbia dubbi sulle prenotazioni o desideri segnalare un problema, il nostro team di supporto è pronto ad assisterti.
+        </div>
 
-    <div class="text_support">
-        Puoi contattarci tramite il modulo qui sotto o scriverci direttamente all’indirizzo email <strong><a href="mailto:a.leo88@studenti.unisa.it" id="mailtoemail">supporto-spaziale@mars.com</a></strong>.
+        <div class="text_support">
+            Puoi contattarci tramite il modulo qui sotto o scriverci direttamente all’indirizzo email <strong><a href="mailto:a.leo88@studenti.unisa.it" id="mailtoemail">supporto-spaziale@mars.com</a></strong>.
+        </div>
     </div>
-
-    <!--post con mail to non supporta la corretta formattazione-->
     <form class="form_support" method="get" action="mailto:mailto:a.leo88@studenti.unisa.it">
         <input type="text" name="nome%0D%0A" placeholder="Nome" required>
         <input type="email" name="email" placeholder="Email" required>
@@ -66,9 +71,8 @@
         <div style="color: white; display: block; margin-bottom: 0.5%; margin-top: 0.5%">Messaggio</div>
         <textarea type="text" name="body" style="height: 25%"></textarea>
         <input type="submit" value="Invia" style="color: white; display: block; margin-bottom: 0.5%; margin-top: 0.5%">
-        <!--bisgona usare una specie di concatenazione magari con js per i campi email tel e num_ordine per aggiungerli al body-->
     </form>
-
+    <?php include_once '../html/Footer.html'?>
 </main>
 </body>
 </html>
