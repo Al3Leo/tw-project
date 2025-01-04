a3bbdc5fd3bcd6b59869102e5595d81d8408f1b6
Alessio Leo <a.leo88@studenti.unisa.it>
2025-01-04 15:45:16 +0100

    php per la registrazione funzionante

--- src/bozze/temp_RegistraUtente.php
+++ src/bozze/temp_RegistraUtente.php
@@ -8,8 +8,9 @@ $password = 'tw2024';
 $connection_string = "host=$host port=$port dbname=$db user=$username password=$password";
 $db_connection =pg_connect($connection_string) or die('impossibile connettersi al databse<br>'.pg_last_error());
 //get id disponibile
-$query = "SELECT numero_visite FROM info_server";
-$user_id = pg_query($db_connection, $query) or die('Errore durante la lettura del id: ' . pg_last_error());
+$query = "SELECT id_utenti FROM info_server";
+$ret = pg_query($db_connection, $query) or die('Errore durante la lettura del id: ' . pg_last_error($db_connection));
+$user_id=pg_fetch_row($ret)[0];
 //query add
 if(!empty($_POST)) {
     $user_nome = $_POST['user_nome'];
@@ -22,15 +23,16 @@ if(!empty($_POST)) {
     if ($result) {
         $result2 = pg_execute("inserisciUtente", array($user_id, $user_nome, $user_cognome, $user_username, $user_password, $user_sesso));
         if(!$result2)
-            echo pg_last_error($db_connection);
+            echo "mino".pg_last_error($db_connection);
     }else
-        echo pg_last_error($db_connection);
+        echo "jo".pg_last_error($db_connection);
 }
 //update numero utenti registrati
-$sql = "UPDATE info_server SET numero_utenti = numero_utenti + 1";
-$result = pg_query($db_connection, $sql);
-if ($result)
-    pg_last_error();
+$sql = "UPDATE info_server SET numero_utenti = numero_utenti+1";
+pg_query($db_connection, $sql) or die('Errore update numero utenti: '.pg_last_error());
+//update id
+$query = "UPDATE info_server SET id_utenti = id_utenti+1";
+pg_query($db_connection, $query) or die('Errore durante update id: ' . pg_last_error($db_connection));
 //close
 pg_close($db_connection);
 ?>
