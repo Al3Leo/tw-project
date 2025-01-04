56c81533044df78502e451c67ac9c3eb7e195953
Alessio Leo <a.leo88@studenti.unisa.it>
2025-01-04 16:18:52 +0100

    update file vari

--- src/php/PaginaBianca.php
+++ src/php/PaginaBianca.php
@@ -16,45 +16,45 @@
             display: flex;
             flex-direction: column;
         }
-/* sezione login */
-    .popup{
-        position: absolute;
-        margin-top: 10em;
-        margin-left: 35%;
-        color: white;
-        height: auto;
-        width: 25%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        font-size: 90%;
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-        background-color: #8546ff;
-        border: 3px solid #1d0b42;
-    }
-    .popuphead{
-        background-color: #5000fd;
-        display: flex;
-        width: 100%;
-        justify-content: space-between;
-        margin-bottom: 1%;
-    }
-    #loginpopup{
-        display: none;
-    }
-    #closePopup{
-        color: #ffffff;
-        font-size: 80%;
-        text-align: center;
-        background-color: red;
-        width: 3%;
-        height: auto;
-        align-self: center;
-        cursor: pointer;
-    }
-    #popupMain{
-        margin-top: 7%;
-    }
+        /* sezione login */
+        .popup{
+            position: absolute;
+            margin-top: 10em;
+            margin-left: 35%;
+            color: white;
+            height: auto;
+            width: 25%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            font-size: 90%;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+            background-color: #8546ff;
+            border: 3px solid #1d0b42;
+        }
+        .popuphead{
+            background-color: #5000fd;
+            display: flex;
+            width: 100%;
+            justify-content: space-between;
+            margin-bottom: 1%;
+        }
+        #loginpopup{
+            display: none;
+        }
+        #closePopup{
+            color: #ffffff;
+            font-size: 80%;
+            text-align: center;
+            background-color: red;
+            width: 3%;
+            height: auto;
+            align-self: center;
+            cursor: pointer;
+        }
+        #popupMain{
+            margin-top: 7%;
+        }
     </style>
     <meta name="Author" content="Gruppo05"/>
     <base href="../"/>
@@ -62,44 +62,45 @@
     <script src="js/TopMenu.js"></script>
 </head>
 <body>
-    <!--mi sa che il base href vale solo per html e non php-->
-    <?php include_once '../html/TopMenu.html'?>
+<!--mi sa che il base href vale solo per html e non php-->
+<?php include_once '../html/TopMenu.html'?>
 
-        <div class="popup" id="loginpopup">
-            <header class="popuphead">Login
-                <span id="closePopup" onclick="closeLoginPopup()">x</span>
-            </header>
-            <main id="popupMain">
-                    <form method="post">
-                        <label>Username</label><br>
-                        <input type="text" name="username" placeholder="Username" required>
-                        <br>
-                        <label>Password</label><br>
-                        <input type="password" name="password" placeholder="Password" required>
-                        <br>
-                        <label><a href="RecuperaPassword.php">Password smarrita?</a> </label>
-                        <br>
-                        <input type="submit" value="Login" style="margin-left: 37%; margin-top: 10%; margin-bottom: 7%; border: 1px solid black; border-radius: 5px; background-color: #adadad; color: white">
-                    </form>
-            </main>
-        </div>
+<div class="popup" id="loginpopup">
+    <header class="popuphead">Login
+        <span id="closePopup" onclick="closeLoginPopup()">x</span>
+    </header>
+    <main id="popupMain">
+        <form method="post">
+            <label>Username</label><br>
+            <input type="text" name="username" placeholder="Username" required>
+            <br>
+            <label>Password</label><br>
+            <input type="password" name="password" placeholder="Password" required>
+            <br>
+            <label><a href="RecuperaPassword.php">Password smarrita?</a> </label>
+            <br>
+            <input type="submit" value="Login" style="margin-left: 37%; margin-top: 10%; margin-bottom: 7%; border: 1px solid black; border-radius: 5px; background-color: #adadad; color: white">
+        </form>
+    </main>
+</div>
 
-        <script>
-            function openLoginPopup(){
-                document.getElementById("loginpopup").style.display = "flex";
-            }
-            function closeLoginPopup(){
-                document.getElementById("loginpopup").style.display = "none";
-            }
-        </script>
+<script>
+    function openLoginPopup(){
+        document.getElementById("loginpopup").style.display = "flex";
+    }
+    function closeLoginPopup(){
+        document.getElementById("loginpopup").style.display = "none";
+    }
+</script>
 
 
-        <main>
-            <form method="get" action="php/ConfermaDinamica.php">
-                <input type="hidden" name="confermaDinamica" value="confermato dinamicamente">
-                <button type="submit">provami</button>
-            </form>
-        </main>
-    <?php include_once '../html/Footer.html'?>
+<main>
+    <form method="get" action="php/ConfermaDinamica.php">
+        <input type="hidden" name="confermaDinamica" value="confermato dinamicamente">
+        <button type="submit">provami</button>
+    </form>
+</main>
+
+<?php include_once '../html/Footer.html'?>
 </body>
 </html>
--- src/php/Supporto.php
+++ src/php/Supporto.php
@@ -2,99 +2,15 @@
 <head>
     <meta charset="UTF-8">
     <title>Supporto Spaziale</title>
-    <style>
-        html, body {
-            margin: 0;
-            padding: 0;
-        }
-        body {
-            background-image: linear-gradient(75deg, rgb(14,11,19), rgb(82, 0, 124));
-            background-repeat: no-repeat;
-            background-size: cover;
-            display: flex;
-            flex-wrap: wrap;
-            flex-direction: column;
-            font-family: -apple-system, BlinkMacSystemFont, sans-serif;
-        }
-        #text_container{
-            display: block;
-            width: 40vw;
-        }
-        .text_support{
-            color: white;
-            display: block;
-            font-size: 1.2em;
-            width: auto;
-            text-align: center;
-            padding-bottom: 3%;
-        }
-        input, textarea{
-            display: block;
-            margin-bottom: 1%;
-            height: 2rem;
-            width: 40rem;
-            border-radius: 7px;
-        }
-        textarea{
-            height: 7rem !important;
-        }
-        #mailtoemail{
-            color: white;
-        }
-        #mailtoemail:hover{
-            color: #1fe100;
-        }
-        main{
-            min-height: 100%;
-            width: 100%;
-            display: flex;
-            align-items: center;
-            flex-direction: column;
-            flex-wrap: nowrap;
-            height: 80%;
-        }
-    </style>
     <meta name="Author" content="Gruppo05"/>
     <base href="../"/>
     <link rel="icon" href="img/favicon.png" type="image/png/">
+    <link rel="stylesheet" type="text/css" href="css/Supporto.css"/>
     <script src="js/TopMenu.js"></script>
 </head>
 <body>
 <?php include_once '../html/TopMenu.html' ?>
-<main>
-    <div id="text_container">
-        <div class="text_support">
-            Hai bisogno di aiuto?<br>Siamo qui per rispondere a ogni tua domanda!
-        </div>
-
-        <div class="text_support">
-            Che tu stia cercando informazioni sui nostri viaggi spaziali, abbia dubbi sulle prenotazioni o desideri segnalare un problema, il nostro team di supporto è pronto ad assisterti.
-        </div>
-
-        <div class="text_support">
-            Puoi contattarci tramite il modulo qui sotto o scriverci direttamente all’indirizzo email <strong><a href="mailto:a.leo88@studenti.unisa.it" id="mailtoemail">supporto-spaziale@mars.com</a></strong>.
-        </div>
-    </div>
-    <form class="form_support" method="get" action="mailto:mailto:a.leo88@studenti.unisa.it">
-        <input type="text" name="nome" placeholder="Nome" required>
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="text" name="subject" placeholder="Oggetto">
-        <input type="tel" name="telefono" placeholder="Telefono (Facoltativo)">
-        <input type="text" name="num_ordine" placeholder="N.Ordine (Facoltativo)">
-        <div style="color: white; display: block; font-weight: bold; margin-top: 0.7em; margin-bottom: 0.2em;">Messaggio:</div>
-        <textarea type="text" name="body" style="height: 25%; resize: none"></textarea>
-        <input type="hidden" name="confermaDinamica" value="Supporto richiesto con successo">
-        <input type="submit" value="Invia!" style="color: #ffffff;
-            background-color: #00c4ff;
-            margin-top: 2%;
-            width: auto;
-            height: auto;
-            padding: 1.2%;
-            font-size: x-large;
-            margin-left: 45%;
-            border: 2px solid #ffffff;">
-    </form>
-</main>
+<?php require_once '../html/Supporto.html' ?>
 <?php include_once '../html/Footer.html'?>
 </body>
 </html>
