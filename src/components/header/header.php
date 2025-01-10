52bad0d03dfbc758f9e7059d0b9972b7c6459b5e
rossellapale <r.pale1@studenti.unisa.it>
2025-01-10 18:50:39 +0100

    modifiche link footer

--- src/components/header/header.php
+++ src/components/header/header.php
@@ -1,12 +1,10 @@
 <?php
 
-
 /* Abilita log */
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-/* Utilizza percorsi relativi */
 require_once __DIR__ . '/../topmenu/TopMenu.php';
 require_once __DIR__ . '/../login/PopupLogin.html';
 require_once __DIR__ . '/../cart/cart.php';
