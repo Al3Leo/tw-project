5eb66f3be8b54f35b3c2ec4f4124672a0f7aeb18
rossellapale <r.pale1@studenti.unisa.it>
2025-01-13 14:38:56 +0100

    ho rimodificato cart perche non mi funzionava non so perche, confirm page

--- src/backend/addToCart.php
+++ src/backend/addToCart.php
@@ -5,53 +5,56 @@
  * in fine il carrello è salvato in stringa con encoder json nei cookie
  */
 session_start();
+
 // Connessione al database
 $host = 'localhost';
-$port = '9999';
+$port = '5432';
 $db = 'GRUPPO05';
 $username = 'www';
 $password = 'tw2024';
 $connection_string = "host=$host port=$port dbname=$db user=$username password=$password";
 $db_connection = pg_connect($connection_string) or die('Impossibile connettersi al database<br>' . pg_last_error());
 
-// Query per ottenere nome e prezzo del prodotto
+
 $id = $_GET['id'];
-$query = "SELECT nomeevento FROM viaggio WHERE idevento='$id'";
+$query = "SELECT nomeevento, prezzoevento FROM viaggio WHERE idevento='$id'";
+
+
+
 $return = pg_query($db_connection, $query) or die('Errore: ' . pg_last_error($db_connection));
-$prodotto = pg_fetch_row($return)[0];
-$query = "SELECT prezzoevento FROM viaggio WHERE idevento='$id'";
-$return = pg_query($db_connection, $query) or die('Errore: ' . pg_last_error($db_connection));
-$prezzo = pg_fetch_row($return)[0];
+$prodotto = pg_fetch_assoc($return);
 pg_close($db_connection);
 
 // Se il carrello non esiste nei cookie lo crea vuoto altrimenti decoder
 if (isset($_COOKIE['cart'])) {
-    $cart = json_decode($_COOKIE['cart'], true); //associativo? true
+    $cart = json_decode($_COOKIE['cart'], true);
 } else {
     $cart = [];
 }
 
-// preparo il prodotto come array
+
 $info = array(
-    "nome" => $prodotto,
-    "prezzo" => $prezzo
+    "nome" => $prodotto['nomeevento'] ?? null,
+    "prezzo" => $prodotto['prezzoevento'] ?? null
 );
 
-// check se il prodotto è già presente nel carrello
-$duplicato = false;
-foreach ($cart as $index_main_array => $sottoarray) {
-    // Confronta il nome del prodotto
-    if ($sottoarray['nome'] == $prodotto) {
-        $duplicato = true;
-        break;
-    }
-}
-// se il prodotto non è un duplicato, aggiungilo al carrello
-if (!$duplicato) {
-    $cart[] = $info; // sintassi add at the end
-}
+    $duplicato = false;
+    foreach ($cart as $index_main_array => $sottoarray) {
+        if ($sottoarray['nome'] == $prodotto['nomeevento']) {
+            $duplicato = true;
+            break;
+        }
 
-setcookie('cart', json_encode($cart), time() + 3600); // Imposta il cookie con il carrello aggiornato
-// ritorna al carrello
-header("Location: " . $_SERVER['HTTP_REFERER']);
+    }
+
+// se il prodotto non è un duplicato, aggiungilo al carrello
+    if (!$duplicato) {
+        $cart[] = $info;
+    }
+
+    setcookie('cart', json_encode($cart), time() + 3600, "/");
+
+    header("Location: " . $_SERVER['HTTP_REFERER']);
+    exit();
+
 ?>
