de4fb542d5936da4e5b7b6183cac4ac03713b714
Emanuele Tocci <emato03.felix@gmail.com>
2025-01-24 22:23:24 +0100

    implementazione parziale redirect 404

---
+++ src/pages/error/error.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Oh no, ERROR 404!</h1>
+    <p>Maybe a black hole ate all our beautiful pages.</p>
+    <p>
+        Don't worry, we'll fix it! Meanwhile here is a waiting song
+    </p>
+    <audio controls autoplay>
+        <source src="waiting.mp3">
+        audio not supported
+    </audio>
+</body>
+
+</html>
