3d9dc332d22a089ddf6268414a479d5317c9fc66
rossellapale <r.pale1@studenti.unisa.it>
2025-01-24 16:18:00 +0100

    mdifica carrello per eliminazione

--- src/components/cart/cart.php
+++ src/components/cart/cart.php
@@ -16,7 +16,7 @@
     background-color: transparent;
 }
  </style>
-<div id="carrello scroll" class="popup d-flex flex-column" style="overflow: auto; padding:0.3rem">
+<div id="carrello" class="popup d-flex flex-column" style="overflow: auto; padding:0.3rem">
     <div id="remove_popup" style="margin-top: 3%; text-align: right; width: 90%">
         <span style="background-color: red; color: white;" onclick="openCart()">close</span>
     </div>
@@ -39,7 +39,7 @@
                     echo "<td style='text-align: left; width: fit-content'>" . $value['nome'] . "</td>";
                     echo "<td style='width: 10%'></td>";
                     echo "<td style='text-align: right; width: fit-content'>" . $value['prezzo'] . " $</td>";
-                    echo "<td><button onclick='ajax_remove(1)'>&#x1F5D1</button></td>"; // cestino Unicode
+                    echo "<td><button onclick='ajax_remove(" . $value['id'] . ")'>&#x1F5D1</button></td>"; // cestino Unicode
                     echo "</tr>";
                 }
                 if ($tot != 0)
@@ -48,7 +48,7 @@
                     <td><button type='button' id='acquistaButton'>Buy</button></td>
                   </tr>";
                 else {
-                    echo "<tr><td colspan='4'>Il carrello è vuoto.</td></tr>";
+                    echo "<tr><td colspan='4'>Choose your next destination</td></tr>";
                 }
             }
         ?>
@@ -57,7 +57,7 @@
 
 <script>
 document.getElementById('acquistaButton').addEventListener('click', function() {
-    const cart = <?php echo json_encode($cart); ?>; // dati del carrello dal PHP
+    const cart = <?php echo json_encode($cart); ?>; // passo all'url il carrello codificato in json
     const encodedCart = encodeURIComponent(JSON.stringify(cart));
     window.location.href = '../src/components/Stripe/Checkout.php?cart='+encodedCart;
 });
@@ -72,7 +72,21 @@ window.onload = function() {
     }
 };
 function svuotaCarrelloFrontend() { 
-    document.getElementById('carrello_tbl').innerHTML = `<tr> <th style="text-align: left">Where</th> <th></th> <th>Price</th> </tr> <tr><td colspan="4">Choose your nex destination</td></tr>`;
+    document.getElementById('carrello_tbl').innerHTML = `<tr> <th style="text-align: left">Where</th> <th></th> <th>Price</th> </tr> <tr><td colspan="4">Choose your next destination</td></tr>`;
 }
+
+function ajax_remove(id) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'backend/RemoveItemFromCart.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            // Aggiorna l'interfaccia utente dopo aver rimosso l'elemento
+            document.location.reload(true);
+        }
+    };
+    xhr.send('id=' + id);
+}
+
 
 </script>
