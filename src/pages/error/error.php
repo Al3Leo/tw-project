82d92bf6a74db85d89c3ad6ae07684f31b1224d9
Emanuele Tocci <emato03.felix@gmail.com>
2025-01-25 22:16:11 +0100

    modifiche 404

--- src/pages/error/error.php
+++ src/pages/error/error.php
@@ -2,21 +2,38 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Error</title>
+    <style>
+        @import "/src/assets/css/Global.css";
+        body {
+            background-image: url("/src/assets/images/404.jpg");            
+            background-repeat: no-repeat;
+            background-size: cover;
+            height: 100vh;
+        }
+
+        div {
+            padding: 30px;
+        }
+
+        audio {
+            margin: 0 auto;
+        }
+    </style>
 </head>
 
 <body>
-    <h1>Oh no, ERROR 404!</h1>
-    <p>Maybe a black hole ate all our beautiful pages.</p>
-    <p>
-        Don't worry, we'll fix it! Meanwhile here is a waiting song
-    </p>
-    <audio controls autoplay>
-        <source src="waiting.mp3">
-        audio not supported
-    </audio>
+    <div>
+        <h1>Oh no, ERROR 404!</h1>
+        <h2>Maybe a black hole ate all our beautiful pages.</h2>
+        <p>
+            Don't worry, we'll fix it! Meanwhile here is a waiting song.
+        </p>
+        <audio controls autoplay>
+            <source src="/src/assets/audio/waiting.mp3" type="audio/mpeg">
+            audio not supported
+        </audio>
+    </div>
 </body>
 
 </html>
