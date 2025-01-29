4480d96d9c064878d840cedb49f4a660082fc686
Emanuele Tocci <emato03.felix@gmail.com>
2025-01-29 14:17:24 +0100

    aggiornamenti homepage

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,19 +1,26 @@
 /* Countdown manager*/
-let countdown = new Date("Feb 27, 2025 00:00:00").getTime();
+let countdown = new Date("Feb 27, 2025 00:00:00").getTime();  //data di scadenza
 if (document.getElementById("main__left__countdown__days")){
-let x = setInterval(function () {
-  let now = new Date().getTime();
-  let distance = countdown - now;
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+let x = setInterval(() => {
+  let now = new Date().getTime(); // prelevo l'ora attuale
+  let distance = countdown - now; // calcolo la distanza con la data prefissata in ms
+   
+  if (distance < 0){  // se il tempo é esaurito
+    clearInterval(x);
+    document.getElementById("main__left").innerHTML = ""; // rimuovo tutto il contenuto della sezione di sinistra 
+  }
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));  // converto ms in giorni
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));  // convert ms in ore
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));  // converto ms in m
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);  // converto ms in s
   document.getElementById("main__left__countdown__days").innerHTML = days;
   document.getElementById("main__left__countdown__hours").innerHTML = hours;
   document.getElementById("main__left__countdown__minutes").innerHTML = minutes;
-  document.getElementById("main__left__countdown__seconds").innerHTML = seconds;}, 1000); //Repeat every sec
+  document.getElementById("main__left__countdown__seconds").innerHTML = seconds;}, 1000); //La funzione viene ripetuta ogni secondo
 clearInterval();
 }
+
 /*FAQ*/
 const faqItems = document.querySelectorAll('.faq__item');
 faqItems.forEach(item => {
