da953b5ff8ba1d462e1869782930b7d2c59c8ba7
ClaudiaMontefusco3 <c.montefusco6@studenti.unisa.it>
2025-02-05 13:02:40 +0100

    Update footer.php

--- src/components/footer/footer.php
+++ src/components/footer/footer.php
@@ -1,19 +1,19 @@
 <footer>
   <div id="footer__container">
-  <div class="footer__div_container d-flex justify-content-around align-items-center flex-wrap">
+  <div class="footer__div_container d-flex justify-content-around align-items-center flex-wrap"> <!--classi per gestire la disposizione degli elementi-->
     <div class="footer__contenuto_container">
     <a href="<?php echo $_SERVER['PHP_SELF'];?>"> <!--svg per il logo-->
-        <svg width="158" height="62" viewBox="0 0 158 62" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect width="158" height="62" />
+        <svg width="158" height="62" viewBox="0 0 158 62" fill="none" xmlns="http://www.w3.org/2000/svg"> <!--usato per il logo footer-->
+            <rect width="158" height="62" /> <!--sfondo per il logo-->
             <rect width="165" height="102" transform="translate(-7 -13)" />
-            <defs>
+            <defs> <!--per elementi grafici-->
                 <filter id="shadow" x="-50%" y="-50%" width="200%" height="200%">
-                    <feDropShadow dx="3" dy="2" stdDeviation="0" flood-color="rgba(212, 187, 221, 0.5)" />
+                    <feDropShadow dx="3" dy="2" stdDeviation="0" flood-color="rgba(212, 187, 221, 0.5)" /> <!--creo effetti speciali sugli elementi del footer-->
                 </filter>
-            </defs>
+            </defs> 
             <text fill="#F9F4F4" xml:space="preserve" style="white-space: pre; font-family: 'Montserrat', serif; 
             font-weight: 650; font-style: normal;" family="Montserrat" font-size="42" filter="url(#shadow)" letter-spacing="0em">
-                <tspan x="7" y="54.3636">SPACE&#10;</tspan>
+                <tspan x="7" y="54.3636">SPACE&#10;</tspan> <!--modifico parti del testo o le imposto su più righe-->
             </text>
             <text fill="#F7E951" xml:space="preserve" style="white-space: pre; font-family: 'Montserrat', serif; 
             font-weight: 602; font-style: normal;" font-size="20" font-weight="350" letter-spacing="0.3em">
